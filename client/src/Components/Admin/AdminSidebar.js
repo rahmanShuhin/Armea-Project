90b8f313cf3ded43236827784b69312d8660878a
rahmanShuhin <suhin4000@gmail.com>
2021-03-06 18:01:03 +0600

    dashboard updated

--- client/src/Components/Admin/AdminSidebar.js
+++ client/src/Components/Admin/AdminSidebar.js
@@ -13,7 +13,18 @@ import report_logo from "../../images/report_logo.png";
 import blog_logo from "../../images/blog_logo.png";
 import home_logo from "../../images/home_logo.png";
 import post_logo from "../../images/post_logo.png";
+import TreeView from "@material-ui/lab/TreeView";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
+import TreeItem from "@material-ui/lab/TreeItem";
+import { makeStyles } from "@material-ui/core/styles";
+const useStyles = makeStyles({
+  root: {
+    display: "flex",
+  },
+});
 const AdminSidebar = ({ step, setStep }) => {
+  const classes = useStyles();
   return (
     <div className="adminSide">
       <div>
@@ -24,35 +35,50 @@ const AdminSidebar = ({ step, setStep }) => {
           <img src={dash_icon} alt="" />
           Tablero
         </div>
+
         <div className={step === 2 && "active"} onClick={() => setStep(2)}>
           <img src={category_logo} alt="" />
-          Categorías
+          <TreeView
+            className={classes.root}
+            defaultCollapseIcon={<ExpandLessIcon></ExpandLessIcon>}
+            defaultExpandIcon={<ExpandMoreIcon />}
+          >
+            <TreeItem nodeId="1" label="Categorías">
+              <div>Nueva Categoría</div>
+              <div>Todas las Categorías</div>
+            </TreeItem>
+          </TreeView>
         </div>
+
+        {/* <div className={step === 2 && "active"} onClick={() => setStep(2)}>
+          <img src={category_logo} alt="" />
+          Categorías
+        </div> */}
         <div className={step === 3 && "active"} onClick={() => setStep(3)}>
           <img src={products_logo} alt="" />
           Productos
         </div>
-        <div className={step === 3 && "active"} onClick={() => setStep(3)}>
+        <div className={step === 4 && "active"} onClick={() => setStep(3)}>
           <img src={coupon_logo} alt="" />
           Cupones
         </div>
-        <div className={step === 3 && "active"} onClick={() => setStep(3)}>
+        <div className={step === 5 && "active"} onClick={() => setStep(3)}>
           <img src={client_logo} alt="" />
           Clientes
         </div>
-        <div className={step === 3 && "active"} onClick={() => setStep(3)}>
+        <div className={step === 6 && "active"} onClick={() => setStep(3)}>
           <img src={Hamburger_logo} alt="" />
           Pedidos
         </div>
-        <div className={step === 3 && "active"} onClick={() => setStep(3)}>
+        <div className={step === 7 && "active"} onClick={() => setStep(3)}>
           <img src={report_logo} alt="" />
           Reportes de Ventas
         </div>
-        <div className={step === 3 && "active"} onClick={() => setStep(3)}>
+        <div className={step === 8 && "active"} onClick={() => setStep(3)}>
           <img src={blog_logo} alt="" />
           Blog
         </div>
-        <div className={step === 3 && "active"} onClick={() => setStep(3)}>
+        <div className={step === 9 && "active"} onClick={() => setStep(3)}>
           <img src={home_logo} alt="" />
           Configurar Home
         </div>
@@ -68,7 +94,7 @@ const AdminSidebar = ({ step, setStep }) => {
           <img src={post_logo} alt="" />
           Correos
         </div>
-        <div className={step === 4 && "active"} onClick={() => setStep(4)}>
+        <div className={step === 11 && "active"} onClick={() => setStep(4)}>
           <img src={setting_logo} alt="" />
           Ajustes
         </div>
